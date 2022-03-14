2feac5d8410158333818b5174d1165299057a44d
Joohyun Lee <leejoohyun_my@naver.com>
2022-03-14 10:20:15 +0900

    AccountDAO-findBalanceByAccountNo(계좌잔액반환) 구조잡기

--- 06-jdbc-bank-project/src/model/AccountDAO.java
+++ 06-jdbc-bank-project/src/model/AccountDAO.java
@@ -1,3 +1,4 @@
+//껍데기 역할
 package model;
 
 import java.sql.Connection;
@@ -22,12 +23,16 @@ public class AccountDAO {
 		if(rs!=null)
 			rs.close();
 		closeAll(pstmt, con);
-	}
-	/*
-	 *  계좌 개설하는 메서드 
-	 *  예외흐름 : 초기 납입액이 1000원 미만일 경우 CreateAccountException을 발생시키고 전파한다 
-	 *  
+	}//AccountDAO
+	
+	/**
+	 * 계좌 개설하는 메서드 
+	 * 예외흐름 : 초기 납입액이 1000원 미만일 경우 CreateAccountException을 발생시키고 전파한다 
+	 * @param accountVO
+	 * @throws SQLException
+	 * @throws CreateAccountException
 	 */
+	
 	public void createAccount(AccountVO accountVO) throws SQLException, CreateAccountException {
 		if(accountVO.getBalance()<1000)
 			throw new CreateAccountException("계좌 개설시 초기 납입금은 1000원 이상이어야 합니다");
@@ -45,6 +50,28 @@ public class AccountDAO {
 			closeAll(pstmt, con);
 		}
 	}
+	
+	/**
+	 * <<findBalanceByAccountNo>>
+	 * 계좌의 잔액을 조회하는 메서드
+	 * 계좌번호에 해당하는 계좌가 없으면 AccountNotFoundException을 발생시키고 전파한다 
+	 * 계좌번호에 해당하는 계좌가 존재하되, 비비밀번호가 일치하지 않으면 NotMatchedPasswordException을 발생시키고 전파한다
+	 * 계좌번호에 해당하는 계좌가 존재하고 비밀번호가 일치하면 잔액(balance)를 반환한다 
+	 * @param accountNo
+	 * @param password
+	 * @return balance
+	 * @throws SQLException
+	 * @throws AccountNotFoundException
+	 * @throws NotMatchedPasswordException
+	 */
+	
+	public int findBalanceByAccountNo(String accountNo,String password)throws SQLException,AccountNotFoundException,NotMatchedPasswordException {
+		int balance=0;
+		return balance;
+	}//findBalanceByAccountNo
+	
+	
+	
 }
 
 
--- 06-jdbc-bank-project/src/test/TestUnit2.java
+++ 06-jdbc-bank-project/src/test/TestUnit2.java
@@ -14,12 +14,10 @@ public class TestUnit2 {
 			
 			//3.잔액조회 정상흐름 실행 		
 			System.out.println("잔액조회:"+ dao.findBalanceByAccountNo("1", "1234"));
-		}catch (AccountNotFoundException e) {
+		}catch (AccountNotFoundException e) {//ex. 11 계좌번호에 해당하는 계좌가 존재하지 않습니다
 				System.out.println(e.getMessage());			
-		}catch (NotMatchedPasswordException e) {			
-			System.out.println(e.getMessage());			
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+		}catch (NotMatchedPasswordException e) { //패스워드 일치하지 않을 떄			
+			System.out.println(e.getMessage()); //ex. 계좌의 패스워드가 일치하지 않습니다
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} 
