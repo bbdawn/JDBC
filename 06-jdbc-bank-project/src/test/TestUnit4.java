31bbf86ba06b4ae7aecb9f3c3b84239831ae9cda
Joohyun Lee <leejoohyun_my@naver.com>
2022-03-14 09:42:01 +0900

    bank project

---
+++ 06-jdbc-bank-project/src/test/TestUnit4.java
@@ -0,0 +1,36 @@
+package test;
+
+//step4 입금 테스트 
+public class TestUnit4 {
+	public static void main(String[] args) {
+		/*
+		try {
+			AccountDAO dao=new AccountDAO();
+			//대안흐름테스트 
+			//정상흐름테스트 			
+			String accountNo="1";//계좌번호 테스트
+			String password="1234";//패스워드 테스트
+			int money=50;//입금액 테스트 
+			System.out.println("입금전 잔액:"+dao.findBalanceByAccountNo(accountNo, password));
+			dao.deposit(accountNo,password,money);
+			System.out.println("입금완료");
+			System.out.println("입금후 잔액:"+dao.findBalanceByAccountNo(accountNo, password));
+		}catch(NoMoneyException e) {
+			System.out.println(e.getMessage());
+		}catch(AccountNotFoundException e) {
+			System.out.println(e.getMessage());
+		}catch(NotMatchedPasswordException e) {
+			System.out.println(e.getMessage());
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		} 
+		*/
+	}
+}
+
+
+
+
+
